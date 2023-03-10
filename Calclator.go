c9ac9e17093611c8e478f81f67c448dfc9e20992
BachhavPriyanka <bachhavpriyanka09@gmail.com>
2023-03-10 16:31:21 +0530

    Added : handleFunction for Calculator and error handle function

--- Calclator.go
+++ Calclator.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
@@ -12,12 +14,37 @@ type Data struct {
 
 var data Data
 
+func errorHandling(w http.ResponseWriter, err error) {
+	fmt.Println(err)
+	http.Error(w, "Internal server error", http.StatusInternalServerError)
+}
+
 func main() {
 
 	http.HandleFunc("/calculate", handleCalculator) //  endpoint
 	http.ListenAndServe("localhost:8080", nil)      // listen and serve
 }
 
-func handleCalculator(w http.ResponseWriter, r *http.Request) {
+func handleCalculate(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		if err := json.NewDecoder(r.Body).Decode(&data); err != nil { // Decoding the request data
+			errorHandling(w, err)
+			http.Error(w, "Error decoding data", http.StatusBadRequest)
+			return
+		}
 
+		switch data.Operation {
+		case "sum":
+			result := calculateSum(data)
+		case "subtract":
+			result := calculateSubtract(data)
+		case "multiplication":
+			result := calculateMultiplication(data)
+		case "division":
+			result := calculateDivision(data)
+		default:
+			http.Error(w, "Invalid operation", http.StatusBadRequest)
+			return
+		}
+	}
 }
