1482b8250137e3aa613bb23671d4400b07352fd4
BachhavPriyanka <bachhavpriyanka09@gmail.com>
2023-03-10 13:47:38 +0530

    Refactored: Added the missing bracket

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import "fmt"
 
 func calculateSum(a, b, c, d int) int {
 	return a * b * c * c
+	//featured added by priyanka
+}
 func calculateSum(x, z, c, d int) int {
 	return x * z * c * d
 	//feature added by aoshin
